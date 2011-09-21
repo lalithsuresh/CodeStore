3716d579d63d7461600217a79d30bfb98da1be95
Lalith Suresh <suresh.lalith@gmail.com>
2011-09-21 22:42:36 +0200

    Adds StorageObject class and logic for applying basis vectors

--- distribute.py
+++ distribute.py
@@ -3,10 +3,10 @@ import os
 import errno
 import struct
 
-class Distributer:
+class Distributor:
     """Given a list of objects, this class distributes them and/or retrieves them"""
 
-    def __init__(self):
+    def create_dirs(self):
         current_dir = os.getcwd()
         for path in conf.DIRS:
             try:
@@ -18,27 +18,49 @@ class Distributer:
     def push_objects_to_stores(self, name, list_of_objects):
         i = 0
         assert len(list_of_objects) == len(conf.DIRS)
+
         for element in map(lambda x: os.path.abspath(x), conf.DIRS):
             assert(len(list_of_objects[i])) == conf.PART_SIZE
             outfile = open(os.path.join(element, name + str(i)), 'wb')
-            outfile.write(struct.pack('I'*len(list_of_objects[i]),*list_of_objects[i]))
+
+            for obj in list_of_objects[i]:
+                for key in obj.get_data_map():
+                    outfile.write (struct.pack ('II'+str(len(obj.get_data_map[key]))+'s', [key, len(obj.get_data_map[key]), obj.get_data_map[key]]))
+
             outfile.close()
             i += 1
 
-    def pull_objects_from_stores(self, name):
+    def pull_objects_from_stores(self, name, num):
         list_of_objects=[]
         i = 0
+        success = 0
         for element in map(lambda x: os.path.abspath(x), conf.DIRS):
-            infile = open(os.path.join(element, name + str(i)), 'rb')
+            try:
+                infile = open(os.path.join(element, name + str(i)), 'rb')
+            except IOError:
+                print "Error opening %s" % (infile.name)
+                continue
+            finally:
+                i += 1
             line = infile.readlines()
             list_of_objects.append(list(struct.unpack('I' * conf.PART_SIZE, line[0].strip())))
-            i += 1
+            success += 1
+            if (success == num):
+                break
 
+        if (num > len(list_of_objects)):
+            print 'Could not retrieve objects from %s nodes' % (num)
+            raise Exception (num)
         return list_of_objects
 
 if __name__ == "__main__":
     dist = Distributer()
-    dist.push_objects_to_stores('objectA', [[1,2],[3,4],[5,6],[7,8]])
-    dist.push_objects_to_stores('objectB', [[2,1],[4,3],[6,5],[8,7]])
-    print dist.pull_objects_from_stores('objectB')
-    print dist.pull_objects_from_stores('objectA')
+    #dist.create_dirs() 
+    #dist.push_objects_to_stores('objectA', [[1,2],[3,4],[5,6],[7,8]])
+    #dist.push_objects_to_stores('objectB', [[2,1],[4,3],[6,5],[8,7]])
+    try:
+        print dist.pull_objects_from_stores('objectB', 3)
+    except Exception as inst:
+        print inst.args
+
+    print dist.pull_objects_from_stores('objectA', 2)
--- split.py
+++ split.py
@@ -1,19 +1,44 @@
 import struct
 import numpy
 import conf
-GLOBAL DEBUG=1
+import copy
+import distribute
+
+class MetaStore:
+      meta_map = {}
+      
+
+class StorageObject:
+
+      def __init__ (self, data, index):
+          self.data_map = {}
+          self.data_map[index] = data
+
+      def get_data_map (self):
+          return self.data_map
+
+      # This method follows XOR semantics
+      def AddObject (self, new_obj):
+          for x in new_obj.data_map:
+              # Assumes that both values held by the key
+              # are the same
+              if (self.data_map.has_key (x)):
+                  del self.data_map[x]
+              else:
+                  self.data_map[x] = new_obj.data_map[x]
+
+
 class Strip:
         """ A class that strips the file into 4 binary pieces"""
         def splitter(self,filename):
                     with open(filename, "rb") as f:
                                 otherfd = open("other.mp3",'wb')                                
                                 byte = f.read(1)
-                                s=""
+                                s=byte
                                 while byte !="":
                                     byte = f.read(1)
                                     s = s + str(byte)
-                                if DEBUG:
-                                    s="12345"
+
                                 #Not considering having less char in s than NUM_PIECES
                                 chunks_size = int(len(s) /conf.NUM_PIECES)
                                 chunks_list = [ ]
@@ -32,12 +57,44 @@ class Strip:
                                 f.close()
                                 otherfd.close()
                                 return chunks_list                                
-        def xorer(self, chunks_list):
-            for x in chunks_list:
-                
 
+
+def ApplyBasisVectors (storage_obj_list):
+    final_list = []
+    for node in conf.BASIS_VECTORS:
+        obj_list = []
+        for vector in node:
+             
+            flag = 0
+            obj = ''
+            for i in range(0,len(vector)):
+                if (vector[i] == 1):
+                    if (flag == 0):
+                        obj = copy.deepcopy (storage_obj_list [i])
+
+                        flag = 1
+                    else:
+                        obj.AddObject (copy.deepcopy (storage_obj_list [i]))
+                        
+            obj_list.append (obj)
+            del obj
+        final_list.append (obj_list)
+
+    return final_list
 
 if __name__ == "__main__":    
+
+    meta = MetaStore ()
+
     strip = Strip()
     myfile="myfile"
-    strip.splitter("Test.mp3")     
+    li = strip.splitter("Test.mp3")
+
+    i = 0
+    storage_obj_list = []
+    for each in li:
+        so = StorageObject (each, i)
+        storage_obj_list.append (so)
+        i += 1
+
+    final_list = ApplyBasisVectors (storage_obj_list)
